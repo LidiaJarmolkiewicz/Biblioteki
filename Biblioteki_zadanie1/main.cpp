da01b2ca48ca3d09a899cc2ed4f3d0500048923e
LidiaJarmolkiewicz <lidiajarmolkiewicz@gmail.com>
2022-09-17 15:22:23 +0200

    delegowanie konstruktorow

--- Biblioteki_zadanie1/main.cpp
+++ Biblioteki_zadanie1/main.cpp
@@ -7,8 +7,9 @@ private:
     int _age=0;
 public:
     Fish(std::string name, std::string colour, int age) : _name(name), _colour(colour), _age(age) {};
-    Fish(std::string name) :Fish(name, {}, 2) {};
+    Fish(std::string name) :Fish(name, {}, 0) {};
     Fish() :Fish( {}, {}, 0) {};
+    Fish(int age) :Fish({}, {}, 0) {};
 
     std::string getName()
     {
@@ -24,12 +25,39 @@ public:
     }
 
 };
+class GoldFish : public Fish
+{
+public:
+    using Fish::Fish;
+    void plynProsto()
+    {
+        std::cout << "plyn prosto" << std::endl;
+    }
 
+};
+class SilverFish :public Fish
+{
+protected:
+    double _tailLength;
+public:
+    SilverFish(std::string name, std::string colour, int age, double tailLength) : Fish(name, colour, age), _tailLength(tailLength) {};
+    double getTailLength()
+    {
+        return _tailLength;
+    }
+};
 int main()
 {
     Fish fish("Kate","yellow",1);
     std::cout << fish.getName() << " " << fish.getColour() << " " << fish.getAge() << std::endl;
 
-    Fish goldFish("Jane");
-    std::cout << goldFish.getName() << " " << goldFish.getColour() << " " << goldFish.getAge();
+    Fish fish1("Jane");
+    std::cout << fish1.getName() << " " << fish1.getColour() << " " << fish1.getAge() << std::endl;
+
+    GoldFish fish3("Goldi","gold",3);
+    fish3.plynProsto();
+    std::cout  << fish3.getName() << " " << fish3.getColour() << " " << fish3.getAge() << std::endl;
+
+    SilverFish fish4("Patty", "silver", 3, 12.6);
+    std::cout << fish4.getName() << " " << fish4.getColour() << " " << fish4.getAge() << " " <<fish4.getTailLength()<<std::endl;
 }
